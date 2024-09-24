727e7cca456008eac7abcb67643bc073a3717a6d
m3hu1 <pathakkmehul@gmail.com>
2024-09-24 14:06:29 +0530

    updated tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,8 @@
-import path from "path"
 import react from "@vitejs/plugin-react"
-import eslint from 'vite-plugin-eslint';
+import eslint from 'vite-plugin-eslint'
 import { defineConfig } from "vite"
- 
+
+// Your Vite configuration...
 export default defineConfig({
-  plugins: [react(),eslint()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  plugins: [react(), eslint()]
 })
