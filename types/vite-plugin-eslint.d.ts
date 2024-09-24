7382512e719224c661a5a27587c298bda767ff22
m3hu1 <pathakkmehul@gmail.com>
2024-09-24 13:56:13 +0530

    updated types folder

--- types/vite-plugin-eslint.d.ts
+++ types/vite-plugin-eslint.d.ts
@@ -1,1 +1,11 @@
-declare module 'vite-plugin-eslint';
+declare module 'vite-plugin-eslint' {
+  import { Plugin } from 'vite';
+
+  interface Options {
+    // Add any options that the plugin supports here
+  }
+
+  function eslint(options?: Options): Plugin;
+
+  export default eslint;
+}
