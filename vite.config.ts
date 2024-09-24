804a260533d87300e269106bacd39be0ac333830
m3hu1 <pathakkmehul@gmail.com>
2024-09-24 14:08:59 +0530

    updated vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,14 @@
 import react from "@vitejs/plugin-react"
 import eslint from 'vite-plugin-eslint'
 import { defineConfig } from "vite"
+import path from 'path' // Import path module
 
 // Your Vite configuration...
 export default defineConfig({
-  plugins: [react(), eslint()]
+  plugins: [react(), eslint()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
